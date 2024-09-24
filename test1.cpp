94aab7eca7342f7d2a2e480f251aac762341959f
4meM <eramosq@unsa.edu.pe>
2024-09-24 10:30:16 -0500

    modificaciones

---
+++ test1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
+#define FOR_LOOP(val,i, len) for (int i = val; i < len; ++i)
+#define REVERSE_FOR_LOPP(i, start, end) for(int i = (end) - 1; i >= start; i--)
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n;
+    cin >> n;
+    vector<int> v(n);
+}
